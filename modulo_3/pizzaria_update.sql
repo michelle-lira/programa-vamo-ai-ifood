2394f9a87434860b13d63ea9f42bc9bd248db243
Michelle Lira <mchll.lira@gmail.com>
2021-06-18 10:18:18 -0300

    Adiciona pasta do módulo 3

---
+++ modulo_3/pizzaria_update.sql
@@ -0,0 +1,4 @@
+INSERT INTO pizzaria.tb_cliente (nome,email,endereco) VALUES
+	 ('Marcelo Lopes','m.lopes@fb.com','av. Rocha, numero 2, Rio Vermelho'),
+	 ('Helena Alencar','hele22@fb.com','av. das Orquídeas, numero 4, Liberdade'),
+	 ('Almeida Santos','santo.almeida@xpto.com','rua das Flores, quadra C, numero 14, Liberdade');
